7bf0d2902ff779d8a008cd1b3f5736fa41da35c0
Deepak Poudel <deepak.poudel077@gmail.com>
2026-09-04 21:53:14 +0545

    redesign featured posts layout with bento grid

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -1,6 +1,5 @@
 import React from "react";
 import BlogLayoutOne from "../Blog/BlogLayoutOne";
-import BlogLayoutTwo from "../Blog/BlogLayoutTwo";
 
 const FeaturedPosts = ({ blogs }) => {
   if (!blogs || blogs.length === 0) {
@@ -12,11 +11,17 @@ const FeaturedPosts = ({ blogs }) => {
       <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
         Featured Posts
       </h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:gap-10 mt-10 sm:mt-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 auto-rows-[250px] sm:auto-rows-[300px] gap-4 sm:gap-6 mt-10 sm:mt-16">
         {blogs.slice(0, 4).map((blog, index) => (
           <article
             key={blog.slug}
-            className={`relative ${index === 0 ? "col-span-2 sm:col-span-3 lg:col-span-2 row-span-2" : "col-span-1"}`}
+            className={`relative ${
+              index === 0
+                ? "sm:row-span-2"
+                : index === 3
+                ? "sm:col-span-2"
+                : ""
+            }`}
           >
             <BlogLayoutOne blog={blog} />
           </article>
